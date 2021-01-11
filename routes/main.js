873e5ace7c4b5b809f719875c7b84dc73e4fc0d5
Mennatallah Hamdy <menna.h.m.elsayed@gmail.com>
2021-01-11 17:44:00 +0200

    sec

--- routes/main.js
+++ routes/main.js
@@ -1,8 +1,38 @@
 const express = require('express');
+const mongoose = require('mongoose')
 const path = require('path');
 const router = express.Router();
 const bodyParser = require('body-parser');
 const app = express();
+
+const doctors = [{
+    id: "12345",
+    password: "0000",
+    name: "Fady"
+},
+{
+    id: "54321",
+    password: "1111",
+    name: "Marwa"
+},
+{
+    id: "98765",
+    password: "2222",
+    name: "Salma"
+},
+{
+    id: "56789",
+    password: "3333",
+    name: "Menna"
+},
+{
+    id: "14785",
+    password: "4444",
+    name: "Mohamed"
+},
+]
+    
+
 
 router.get('/',(req,res)=>{
     res.render('home/first');
@@ -29,6 +59,25 @@ router.get('/questions',(req,res)=>{
 });
 router.get('/login',(req,res)=>{
     res.render('home/login');
+});
+router.post('/login',(req,res)=>{
+    var i=0;
+    for (i =0 ; i<5; i++){
+        if(doctors[i].id == req.body.id){
+            var k = i;
+        }   
+    }
+    if(req.body.id != doctors[k].id){
+        res.redirect('/login');
+    }
+    else{
+        if(req.body.password == doctors[k].password){
+            res.redirect('/chat');
+        }
+        else{
+            res.redirect('/login');
+        }
+    }
 });
 router.get('/chat',(req,res)=>{
     res.render('home/chat');
